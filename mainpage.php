7b7fa7e495389546a83213a46c2965792d05d61a
sighakolli susmitha <73429559+sighakollisusmitha@users.noreply.github.com>
2021-11-06 19:29:06 +0530

    Add files via upload

---
+++ mainpage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Main Page</title>
+    <style>
+        .center {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            border: 5px solid gray;
+            padding: 10px;
+         }
+    </style>
+</head>
+<body>
+    <div class="center">
+        <a href="http://localhost/isaa%20lab%20da/student_register.php" class="btn btn-primary">Student Registartion page</a><br><br>
+        <a href="http://localhost/isaa%20lab%20da/faculty_register.php" class="btn btn-primary">Faculty Registartion page</a><br><br>
+        <a href="http://localhost/isaa%20lab%20da/" class="btn btn-primary">Login Page</a>
+    </div>
+</body>
+</html>
---
+++ student_register.php
@@ -0,0 +1,41 @@
+<?php
+include('db_conn.php');//DB Connection
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+ // Username and Password sent from Form
+ $fullname = mysqli_real_escape_string($conn, $_POST['full']);
+ $username = mysqli_real_escape_string($conn, $_POST['user']);
+ $password = mysqli_real_escape_string($conn, $_POST['pass']);
+
+ $password = md5($password); //Password Encrypted
+ $sql = "INSERT INTO users(role,username,password,name) values('user','$username', '$password','$fullname')";
+ $result = mysqli_query($conn, $sql);
+ echo "Created Successfully!";
+}
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>Student Registration</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <style>
+            body{
+                padding:15px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Student Registration</h1>
+        <form action="<?php $_SERVER['PHP_SELF'];?>" method="post">
+             <label>Fullname</label>
+            <input type="text" name="full"><br/><br/>
+            <label>Username</label>
+            <input type="text" name="user"><br/><br/>
+            <label>Password</label>
+            <input type="password" name="pass"><br/><br/>
+            <input type="submit" name="submit" value="Create" class="btn btn-primary"><br/>
+            <h4>already have a account Login<a href="http://localhost/isaa%20lab%20da/"> Here</a></h4>
+       </form>
+    </body>
+</html>
